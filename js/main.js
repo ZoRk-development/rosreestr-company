af54e4ae6bc6ef02cbe172f7e7f0c96301de6533
ZoRk-development <104717863+ZoRk-development@users.noreply.github.com>
2022-05-18 03:43:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,60 @@
+
 AOS.init({
 });
+
+(function initCounters() {
+  var counters = document.getElementById("counters");
+  numbers = counters.querySelectorAll(".counter_num span");
+  var counterObjects = [];
+  [].forEach.call(numbers, (num, i) => {
+    counterObjects[i] = { "target": parseInt(num.innerText.trim().split(" ").join("")), "value": 0 };
+    num.innerText = 0
+  });
+
+  document.addEventListener("aos:in:start-counters", ({ detail }) => {
+    setTimeout(function countNumbersUp() {
+      [].forEach.call(numbers, (num, i) => {
+        var targetObj = counterObjects[i];
+        targetObj["value"] = targetObj["target"] * 0.0001 + targetObj["value"]
+        result = Math.round(targetObj["value"]);
+        if (result < targetObj["target"]) {
+          setTimeout(countNumbersUp, 100);
+        }
+        else {
+          result = targetObj["target"];
+        }
+        num.innerText = result.toLocaleString("en-US").replace(",", " ");
+      });
+    }, 100);
+  })
+})();
+
+(function initAccordeon() {
+  var accordeon = document.querySelector(".accordeon");
+  var items = accordeon.querySelectorAll(".accordeon-item")
+  activeItem = -1;
+  [].forEach.call(items, (item, i) => {
+    item.addEventListener("click", function(e) {
+      toggleItem(item, i);
+    });
+  });
+  function toggleItem(item, i) {
+    if (i == activeItem) {
+      closeItem(item);
+      activeItem = -1;
+    }
+    else {
+      openItem(item);
+      if (activeItem != -1) {
+        closeItem(items[activeItem]);
+      }
+      activeItem = i;
+    }
+  }
+  function openItem(item) {
+    item.classList.add("active");
+  }
+  function closeItem(item) {
+    item.classList.remove("active");
+  }
+})();
